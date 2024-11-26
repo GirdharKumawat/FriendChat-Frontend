1e1b4b905e3dee3c0555d529c57e20d5730da081
Girdhar Kumawat <girdharkumawat3108@gmail.com>
2024-11-26 12:04:21 +0530

    first test

---
+++ lobby.js
@@ -0,0 +1,37 @@
+const Roomform = document.getElementById('roomForm');
+
+
+Roomform.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const roomName = document.getElementById('roomName').value;
+    const username = document.getElementById('joinUsername').value;
+    
+  
+    
+    const response = await fetch(`http://127.0.0.1:8000/getToken/?room=${roomName}`);
+
+    if (!response.ok) {
+        console.error('Failed to get token');
+        return;
+    }
+    const data = await response.json();
+   
+    let UID = data.UID;
+    let Token = data.token;
+     
+    sessionStorage.setItem('UID', UID);
+    sessionStorage.setItem('token', Token);
+    sessionStorage.setItem('room', roomName);
+    sessionStorage.setItem('name', username);
+    window.open('room.html','_self'); 
+   
+});
+
+
+function showModal(modalId) {
+  document.getElementById(modalId).classList.add("active");
+}
+
+function hideModal(modalId) {
+  document.getElementById(modalId).classList.remove("active");
+}
---
+++ stream.js
@@ -0,0 +1,158 @@
+const APP_ID = "c98d2c61906641b9a4c62e83fb71aa09";
+const TOKEN = sessionStorage.getItem("token");
+const CHANNEL = sessionStorage.getItem("room");
+let UID = sessionStorage.getItem("UID");
+
+let NAME = sessionStorage.getItem("name");
+
+const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+
+let localTracks = [];
+let remoteUsers = {};
+
+let joinAndDisplayLocalStream = async () => {
+  document.getElementById("RoomName").innerText = CHANNEL;
+
+  client.on("user-published", handleUserJoined);
+  client.on("user-left", handleUserLeft);
+
+  try {
+    UID = await client.join(APP_ID, CHANNEL, TOKEN, UID);
+  } catch (error) {
+
+    console.error("Error joining channel", error);
+     
+    window.open('lobby.html', '_self')    
+  }
+
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+
+  let member = await createMember();
+  addUserToGrid(UID, member.name);
+
+
+  localTracks[1].play(`user-${UID}`);
+  await client.publish([localTracks[0], localTracks[1]]);
+};
+
+let handleUserJoined = async (user, mediaType) => {
+  remoteUsers[user.uid] = user;
+  await client.subscribe(user, mediaType);
+
+  //
+  if (mediaType === "video") {
+    let player = document.getElementById(`user-container-${user.uid}`);
+    if (player != null) {
+      player.remove();
+    }
+
+    let member = await getMember(user);
+    addUserToGrid(user.uid, member.name);
+ 
+    user.videoTrack.play(`user-${user.uid}`);
+  }
+
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
+};
+
+let handleUserLeft = async (user) => {
+  delete remoteUsers[user.uid];
+  document.getElementById(`user-container-${user.uid}`).remove();
+};
+
+let leaveAndRemoveLocalStream = async () => {
+  for (let i = 0; localTracks.length > i; i++) {
+    localTracks[i].stop();
+    localTracks[i].close();
+  }
+
+  await client.leave();
+
+  deleteMember();
+  window.open("lobby.html", "_self");
+};
+
+let toggleCamera = async () => {
+  if (localTracks[1].muted) {
+    await localTracks[1].setMuted(false);
+     cameraBtn.classList.remove("danger");
+  } else {
+    await localTracks[1].setMuted(true);
+    cameraBtn.classList.add("danger");
+  }
+};
+
+let toggleMic = async () => {
+  if (localTracks[0].muted) {
+    await localTracks[0].setMuted(false);
+    micBtn.classList.remove("danger");
+  } else {
+    await localTracks[0].setMuted(true);
+    micBtn.classList.add("danger");
+  }
+};
+
+let createMember = async () => {
+  let response = await fetch("http://127.0.0.1:8000/createMember/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name: NAME, room: CHANNEL, UID: UID }),
+  });
+  let member = await response.json();
+  return member;
+};
+
+let getMember = async (user) => {
+  let response = await fetch(`http://127.0.0.1:8000/getMember/?UID=${user.uid}&room=${CHANNEL}`);
+
+  let member = await response.json();
+  return member;
+};
+
+let deleteMember = async () => {
+  let response = await fetch("http://127.0.0.1:8000/deleteMember/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name: NAME, room: CHANNEL }),
+  });
+  let member = await response.json();
+};
+
+window.addEventListener("beforeunload", deleteMember);
+
+joinAndDisplayLocalStream();
+
+let leaveBtn = document.getElementById("leave-btn");
+let cameraBtn = document.getElementById("camera-btn");
+let micBtn = document.getElementById("mic-btn");
+
+leaveBtn.addEventListener("click", leaveAndRemoveLocalStream);
+cameraBtn.addEventListener("click",toggleCamera);
+micBtn.addEventListener("click", toggleMic);
+
+ 
+
+let addUserToGrid = (uid, name) => {
+  let userHTML = `
+        <div class="bg-gray-800 rounded-lg relative video-player overflow-hidden ">
+            <div class="z-10 absolute top-2 left-2 flex items-center gap-2 bg-gray-800 px-3 py-1 rounded-full ">
+                <span class="text-white text-sm">${name}</span>
+            </div>
+
+            <div id="user-${uid}" class="w-full h-full">
+            
+            </div>
+        </div>
+          
+        `;
+
+  document
+    .getElementById("videoGrid")
+    .insertAdjacentHTML("beforeend", userHTML);
+};
